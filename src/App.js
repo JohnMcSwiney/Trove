8bdc90ff0c2680ddd799c98dfd820b2dd51dd9b7
jvakhan <97688346+jvakhan@users.noreply.github.com>
2023-02-08 09:13:09 -0700

    Initial Artist Profile
    Using hard coded data.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ArtistProfile from './components/ArtistProfile/ArtistProfile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ArtistProfile />
+
+    </div>
+  ); 
+}
+
+export default App;
---
+++ src/components/ArtistProfile/ArtistAlbum.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function ArtistAlbum(props) {
+
+    function handleMouseOver(event) {
+        
+
+    }    
+
+    return (
+        <div className="slide" id={"slide-" + props.index}><div className="album--cover">
+        <img src={props.cover} className="album--cover" onMouseOver={handleMouseOver} alt="album--cover"/>
+        </div><div className="artist--name"><a href="/" className="song--title">{props.name}</a></div>
+        <h6>{props.releaseDate}  {props.art}</h6></div>
+    )
+}
