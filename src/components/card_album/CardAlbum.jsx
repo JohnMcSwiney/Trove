80a936bd1ba7ea594a70b06018bf9ac8edb6a297
Dan1Nguyen01 <dannguyen062601@gmail.com>
2023-02-13 16:11:53 -0700

    update

--- src/components/card_album/CardAlbum.jsx
+++ src/components/card_album/CardAlbum.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import './style.css';
 
+import albumData from '../../hardcode2'
 
-function CardArtist(props){
-  let picUrl = props.picUrl;  
-  let name = props.name;
+function CardArtist({props}){
+
     return(
-      <div className='bg mx-3 hover:drop-shadow-2xl flex flex-col align-middle justify-center text-center text-ellipsis w-24 rounded-lg shadow-md overflow-hidden  '>
+      <>
+        {
+          albumData.map((album, index)=>(
+            <div className='bg mx-3 hover:drop-shadow-2xl flex flex-col align-middle justify-center text-center text-ellipsis w-24 rounded-lg shadow-md overflow-hidden  '>
                    <div className='rounded-md m-auto  '>
                      <div className='overflow-hidden p-0.5 ' > 
                        <img 
                           className='rounded-md w-full overflow-hidden object-cover h-full ' 
-                          src={props.album.picUrl} 
-                          alt={props.album.name}
+                          src={album.picUrl} 
+                          alt={album.name}
                           />
                      </div>
                    </div>
@@ -21,14 +24,17 @@ function CardArtist(props){
                    <div className='grid grid-flow-row gap-0 h-14 align-middle m-auto'>
                       <a 
                       className='albtxt'>
-                        {props.album.name} 
+                        {album.name} 
                         </a>
                         <a 
                       className='arttxt'>
-                        {props.album.artist} 
+                        {album.artist} 
                         </a>
                     </div>
                    </div>
+          ))
+        }
+      </>
     )
      
 }
