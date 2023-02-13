f187b64eff7bbf26d8d256b81ee5351ff6794bf4
Facelesscactus <113211038+Facelesscactus@users.noreply.github.com>
2023-02-13 12:50:09 -0700

    A Dave State

--- src/AudioPlayer/AudioPlayer.js
+++ src/AudioPlayer/AudioPlayer.js
@@ -13,32 +13,78 @@ const AudioPlayer = () => {
 
     const [isPlaying, setIsPlaying] =  useState(false);
     const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+
 
 
     //reference
     const audioPlayer = useRef(); //reference to the audio player
+    const progressBar = useRef(); //reference to the progress bar
+    const animationRef = useRef(); //reference to the animation
 
+
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+        progressBar.current.max = seconds;
+
+
+
+    }, [audioPlayer?.current?.loadmetadata, audioPlayer?.current?.readyState]);
     
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs / 60);
+        const returnedMinutes = minutes < 10? `0${minutes}` : minutes;
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = seconds < 10? `0${seconds}` : seconds;
+    
+
+        return `${returnedMinutes}:${returnedSeconds}`;
+
+
+    }
+
 
     const togglePlayPause = () => {
 
         const preValue = isPlaying;
 
-
-        
-        
         setIsPlaying(!preValue);
         
         if(!preValue){
             audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
+            
             
         
         }else {
             audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current);
+
 
         }
 
 
+    }
+
+    const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+        
+    }
+
+    const changeRange = () => {
+    
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerCurrentTime();
+
+
+    }
+
+    const changePlayerCurrentTime = () => {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`);
+        setCurrentTime(progressBar.current.value);
     }
 
     return (  
@@ -56,16 +102,16 @@ const AudioPlayer = () => {
 
             {/*current time*/}
 
-            <div className='currentTime'>0:00</div>
+            <div className='currentTime'>{calculateTime(currentTime)}</div>
 
             {/*progress bar*/}
             <div>
-                <input type="range" className='progressBar'/>
+                <input type="range" className="progressBar" defaultValue="0" ref={progressBar} onChange={changeRange} />
             </div>
 
 
             {/*Duration*/}
-            <div className='duration'>{duration}</div>
+            <div className="duration">{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
 
         </div>
     );
