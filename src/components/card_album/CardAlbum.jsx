30183d2dacce3639fa041ca0ba059808b38f7633
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-02-15 13:24:02 -0700

    Added in new component styles

--- src/components/card_album/CardAlbum.jsx
+++ src/components/card_album/CardAlbum.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import './style.css';
+import './CardAlbum.css';
 
-import albumData from '../../hardcode2'
+import albumData from '../../hardcodeAlbums'
 
 function CardArtist({props}){
 
@@ -10,11 +10,14 @@ function CardArtist({props}){
       <>
         {
           albumData.map((album, index)=>(
-            <div className='bg mx-3 flex flex-col align-middle justify-center text-center text-ellipsis w-24 rounded-lg overflow-hidden bg-slate-800  '>
+            <div className='bg mx-3 flex flex-col align-middle 
+                            justify-center text-center text-ellipsis w-24 rounded-lg 
+                            overflow-hidden cont '>
                    <div className='rounded-md m-auto  '>
                      <div className='overflow-hidden p-0.5 ' > 
                        <img 
-                          className='rounded-md w-full overflow-hidden object-cover h-full ' 
+                          className='rounded-md w-full overflow-hidden 
+                                     object-cover h-full' 
                           src={album.picUrl} 
                           alt={album.name}
                           />
@@ -23,11 +26,11 @@ function CardArtist({props}){
 
                    <div className='grid grid-flow-row gap-0 h-14 align-middle m-auto'>
                       <a 
-                      className='albtxt'>
+                      className='albtxt text'>
                         {album.name} 
                         </a>
                         <a 
-                      className='arttxt'>
+                      className='arttxt text'>
                         {album.artist} 
                         </a>
                     </div>
