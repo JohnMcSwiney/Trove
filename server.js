26b823c1bfeef11acd8412224070577460078112
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-02-23 01:17:43 -0700

    made change for displaying users only

--- server.js
+++ server.js
@@ -1,85 +1,81 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport')
-const cookieSession = require('cookie-session');
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const cors = require("cors");
 //For admin
-const userRouter = require('./admin routes/user-route/user-route');
-const albumRouter = require('./admin routes//album-route/album-route');
-const artistRouter = require('./admin routes//artist-route/artist-route');
-const collectionRouter = require('./admin routes//collection-route/collection-route');
-const tastepRouter = require('./admin routes//tastep-route/tastep-route');
-const playlistRouter = require('./admin routes//playlist-route/playlist-route');
-const songRouter = require('./admin routes//song-route/song-route');
+const userRouter = require("./admin routes/user-route/user-route");
+const albumRouter = require("./admin routes/album-route/album-route");
+const artistRouter = require("./admin routes/artist-route/artist-route");
+const collectionRouter = require("./admin routes/collection-route/collection-route");
+
+// const tastepRouter = require("./admin routes/tastep-route/tastep-route");
+const playlistRouter = require("./admin routes/playlist-route/playlist-route");
+const songRouter = require("./admin routes/song-route/song-route");
 
 //For User
-const userlogin = require('./user routes/user-login-route/user');
+const userlogin = require("./user routes/user-login-route/user");
 // const passportSetup = require('./passport')
-const googleAuthRoute= require('./user routes/auth/auth')
+const googleAuthRoute = require("./user routes/auth/auth");
 
 const app = express();
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
-    .then(()=>{
-        console.log('Database connected')
-    })
-    .catch(error=>{
-        console.error('Database connect failed, error: ', error)
-    })
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DB_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.error("Database connect failed, error: ", error);
+  });
 //middle ware
 app.use(express.json());
-app.use((req,res,next) =>{
-    console.log(req.path, req.method);
-    next();
-})
-
-
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //loggin with google
 
-app.use(cookieSession({
+app.use(
+  cookieSession({
     name: "session",
     keys: ["TroveMusic"],
-    maxAge: 24*60*60*100,
-}))
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // app.use('/auth', googleAuthRoute)
 
-
 //FOR USER
-app.use('/api/user', userlogin)
-
-
-
+app.use("/api/user", userlogin);
 
 //FOR ADMIN
 //user
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
 // album
-app.use('/api/albums',albumRouter);
+app.use("/api/albums", albumRouter);
 
 //artist
-app.use('/api/artists',artistRouter);
+app.use("/api/artists", artistRouter);
 
 //collection
-app.use('/api/collections',collectionRouter);
+app.use("/api/collections", collectionRouter);
 
 //taste profile
-app.use('/api/tasteps',tastepRouter);
+// app.use("/api/tasteps", tastepRouter);
 
 // //playlist
-app.use('/api/playlists',playlistRouter);
+app.use("/api/playlists", playlistRouter);
 
 //song
-app.use('/api/songs', songRouter);
+app.use("/api/songs", songRouter);
 
-
-
-app.listen(process.env.PORT, '0.0.0.0', ()=> {
-    console.log(`Listening to port `+process.env.PORT )
-})
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Listening to port ` + process.env.PORT);
+});
--- userController/accountController.js
+++ userController/accountController.js
@@ -1,97 +1,101 @@
-const User  = require('../models/user model/user-model')
-const jwt = require('jsonwebtoken')
-const nodemailer = require('nodemailer')
-require('dotenv').config();
-const createToken = (_id)=> {
-    return jwt.sign({_id: _id},process.env.SECRET, {expiresIn: "1d"})
-}
-
+const User = require("../models/user model/user-model");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "1d" });
+};
 
 //login user
-const loginUser = async (req,res) =>{
-    const {email, password} = req.body;
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-    try{
-        const verify = await User.findOne({email: email})
-        if(!verify.isVerified){
-            return res.status(400).json({err:'Account is not verified'})
-        }
-            
-        const user = await User.login(email, password)
-        //create a token
-        const token = createToken(user._id);
-
-        res.status(200).json({email, token, id:user._id});
-    
-    }catch(err){
-        res.status(400).json({err:err.message});
+  try {
+    const verify = await User.findOne({ email: email });
+    if (verify.isVerified) {
+      return res.status(400).json({ err: "Account is not verified" });
     }
-}
+
+    const user = await User.login(email, password);
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token, id: user._id });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
 
 //sign up user
 
-const signupUser = async(req,res)=>{
+const signupUser = async (req, res) => {
+  const { email, password, isAdmin, displayedName } = req.body;
 
-    const {email, password,isAdmin,displayedName} = req.body;
+  try {
+    const user = await User.signup(email, password);
 
-    try{ 
-        const user = await User.signup(email, password);
+    //create a token
+    const token = createToken(user._id);
 
-        //create a token
-        const token = createToken(user._id);
+    //send email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GOOGLE_USER,
+        pass: process.env.GOOGLE_PASSWORD,
+      },
+    });
 
-        //send email
-        const transporter = nodemailer.createTransport({
-            service:"gmail",
-            auth :{
-                user:process.env.GOOGLE_USER,
-                pass:process.env.GOOGLE_PASSWORD
-            }
-        });
-
-        const mailOptions = {
-            from:process.env.AUTH_EMAIL_ACCOUNT,
-            to:email,
-            subject: "Welcome to TroveMusic!",
-            html:`
+    const mailOptions = {
+      from: process.env.AUTH_EMAIL_ACCOUNT,
+      to: email,
+      subject: "Welcome to TroveMusic!",
+      html: `
             <p>Hi ${email},</p>
             <p>Thank you for signing up for My Awesome App!</p>
             <p>We're thrilled to have you join our community.</p>
             <p>Please click the following link to verify your email address:</p>
             <a href="${process.env.APP_URL_BACKEND}/api/user/verify-email/${user._id}">Verify Here</a>
-          `
-        };
+          `,
+    };
 
-        transporter.sendMail(mailOptions,function(error, info){
-            if(error){
-                console.log(error);
-            }else{
-                console.log('Email sent: ' + info.response);
-            }
-        })
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
 
-        res.status(200).json({email,id: user._id, token, message: "You have successfully sign up !"});
-    }catch(err){
-        res.status(400).json({err:err.message});
+    res
+      .status(200)
+      .json({
+        email,
+        id: user._id,
+        token,
+        message: "You have successfully sign up !",
+      });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+const verifyUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOneAndUpdate({ _id: id }, { isVerified: true });
+    if (!user) {
+      return res.status(404).json("User not found");
     }
-}
-
-const verifyUser = async (req,res)=> {
-    const id =req.params.id;
-    
-    try{
-        const user  = await User.findOneAndUpdate({_id: id}, {isVerified:true})
-        if(!user){
-            return res.status(404).json('User not found');
-        }
-        return res.send('verify-success');
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send('Error verifying email');
-    }
-}
-module.exports={
-    loginUser,
-    signupUser,
-    verifyUser
-}
+    return res.send("verify-success");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Error verifying email");
+  }
+};
+module.exports = {
+  loginUser,
+  signupUser,
+  verifyUser,
+};
