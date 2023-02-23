c68247b2c9b3ebd59ab07e7c917e55c3c76086ca
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-02-23 01:18:10 -0700

    made change for user only

--- src/my account/MyAccount.jsx
+++ src/my account/MyAccount.jsx
@@ -2,6 +2,10 @@ import "./myaccount.css";
 
 import React from "react";
 import { Alert } from "@mui/material";
+import { BiShow } from "react-icons/bi";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+import { Navigate } from "react-router-dom";
+
 const MyAccount = () => {
   const [state, setState] = React.useState(1);
 
@@ -10,148 +14,179 @@ const MyAccount = () => {
 
     console.log(index);
   };
+
+  //states for forms
   const user = localStorage.getItem("user");
-  const userinfo = localStorage.getItem("user") !== null;
-
+  const userInfo = JSON.parse(user);
   const [imgPath, setImagePath] = React.useState("./img/user-demo.png");
+  const [email, setEmail] = React.useState(userInfo.email);
+  const [displayName, setDisPlayName] = React.useState(userInfo.displayName);
+  const [password, setPassword] = React.useState("");
+  const [newPassword, setNewPassword] = React.useState("");
+  const [newEmail, setNewEmail] = React.useState("");
+
+  const [showPassword, setShowPassword] = React.useState(false);
+  const toggleHidden = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div className="container">
-      <h1>Settings</h1>
+    <>
+      {user ? (
+        <div className="container">
+          <h1 className="my-account-header">Settings</h1>
 
-      <div className="my-account">
-        <div className="tabs ">
-          <div className={`${state === 1 ? "active-tab" : "tab"}`}>
-            <div className="account-display" onClick={() => action(1)}>
-              <h3 className="account-header">Account</h3>
-              {/* <div className='account-summary'>
-                            <p >Detail about your personal information</p>
-                          </div> */}
+          <div className="my-account">
+            <div className="tabs ">
+              <div className={`${state === 1 ? "active-tab" : "tab"}`}>
+                <div className="account-display" onClick={() => action(1)}>
+                  <h4 className="account-header">Account</h4>
+                </div>
+              </div>
+
+              <div className={`${state === 2 ? "active-tab " : "tab"}`}>
+                <div className="privacy-display" onClick={() => action(2)}>
+                  <h4 className="privacy-header">Privacy</h4>
+                  {/* <p className='privacy-summary'></p> */}
+                </div>
+              </div>
+
+              <div className={`${state === 3 ? "active-tab" : "tab "}`}>
+                <div className="support-display" onClick={() => action(3)}>
+                  <h4 className="support-header">Support</h4>
+                  {/* <p className='support-summary'></p> */}
+                </div>
+              </div>
             </div>
-          </div>
 
-          <div className={`${state === 2 ? "active-tab " : "tab"}`}>
-            <div className="privacy-display" onClick={() => action(2)}>
-              <h3 className="privacy-header">Privacy</h3>
-              {/* <p className='privacy-summary'>Detail about your personal account information</p> */}
-            </div>
-          </div>
+            <div className="contents">
+              <div className={`${state === 1 ? "active-content" : "content"}`}>
+                <h3>Detail about your personal information</h3>
+                <form>
+                  <div className="inner-form">
+                    <div className="user-img-div">
+                      <img
+                        src={imgPath}
+                        className="user-avatar"
+                        alt="your-avatar"
+                      />
+                    </div>
 
-          <div className={`${state === 3 ? "active-tab" : "tab "}`}>
-            <div className="support-display" onClick={() => action(3)}>
-              <h3 className="support-header">Support</h3>
-              {/* <p className='support-summary'>We can help you here</p> */}
-            </div>
-          </div>
-        </div>
+                    <div>
+                      <label htmlFor="email">Email:</label>
+                      <input
+                        type="email"
+                        id="email"
+                        className="form-control text-dark"
+                        defaultValue={email}
+                        disabled
+                      />
 
-        <div className="contents">
-          <div className={`${state === 1 ? "active-content" : "content"}`}>
-            <form>
-              <div className="inner-form container">
-                <div className="user-img-div">
-                  <img
-                    src={imgPath}
-                    className="user-avatar"
-                    alt="your-avatar"
-                  />
+                      <label htmlFor="name">Display name:</label>
+                      <input
+                        type="text"
+                        id="name"
+                        className="form-control"
+                        defaultValue={displayName}
+                        onChange={(e) => setDisPlayName(e.target.value)}
+                        value={displayName}
+                      />
+                      <label htmlFor="dob">Date of birth:</label>
+                      <input
+                        type="date"
+                        id="dob"
+                        name="dob"
+                        // value={dob}
+                        // onChange={handleDobChange}
+                        className="form-control"
+                      />
+
+                      <button className="btn btn-primary mt-3">Save</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+              <div className={`${state === 2 ? "active-content" : "content"}`}>
+                <h3>Detail about your personal account information</h3>
+
+                <form>
+                  <div>
+                    <label htmlFor="newemail">New email</label>
+                    <input
+                      type="email"
+                      id="newemail"
+                      className="form-control"
+                      placeholder="Change your email here"
+                      onChange={(e) => setNewEmail(e.target.value)}
+                      value={newEmail}
+                    />
+                    <button className="btn btn-primary mt-3 mb-3">
+                      Update Email
+                    </button>
+                  </div>
+                </form>
+
+                <form>
+                  <div>
+                    <label htmlFor="password">Current password</label>
+                    <input
+                      type={"password"}
+                      id="name"
+                      className="form-control"
+                      placeholder="Current password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      value={password}
+                    />
+
+                    <label htmlFor="newpassword">New password</label>
+                    {showPassword ? (
+                      <BiShow onClick={toggleHidden} />
+                    ) : (
+                      <AiOutlineEyeInvisible onClick={toggleHidden} />
+                    )}
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      id="newnewpasswordname"
+                      className="form-control"
+                      placeholder="New password"
+                      onChange={(e) => setNewPassword(e.target.value)}
+                      value={newPassword}
+                    />
+
+                    <button className="btn btn-primary mt-3">
+                      Change Password
+                    </button>
+                  </div>
+                </form>
+              </div>
+
+              <div className={`${state === 3 ? "active-content" : "content"}`}>
+                <h3>We can help you here</h3>
+
+                <div>
+                  <h5>FAQ</h5>
+                  <p>View out Frequently asked questions</p>
                 </div>
 
                 <div>
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    defaultValue={user.email}
-                  />
+                  <h5>Contact us</h5>
+                  <p>
+                    Contact our support team, offer limited to premium users
+                  </p>
+                </div>
 
-                  <label htmlFor="name">Display name</label>
-                  <input
-                    type="text"
-                    id="name"
-                    className="form-control"
-                    defaultValue={user.displayName}
-                  />
-                  <label htmlFor="dob">Date of birth:</label>
-                  <input
-                    type="date"
-                    id="dob"
-                    name="dob"
-                    // value={dob}
-                    // onChange={handleDobChange}
-                    className="form-control"
-                  />
-
-                  <button className="btn btn-primary mt-3">Save</button>
+                <div>
+                  <h5>Report an issue</h5>
+                  <p>Report what's not working well, so we can fix it</p>
                 </div>
               </div>
-            </form>
-          </div>
-
-          <div className={`${state === 2 ? "active-content" : "content"}`}>
-            <form>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                className="form-control"
-                defaultValue={user.email}
-              />
-
-              <div>
-                <label htmlFor="newemail">New email</label>
-                <input
-                  type="email"
-                  id="newemail"
-                  className="form-control"
-                  defaultValue={user.email}
-                />
-
-                <label htmlFor="password">Password</label>
-                <input
-                  type="text"
-                  id="name"
-                  className="form-control"
-                  defaultValue={user.displayName}
-                />
-
-                <label htmlFor="newpassword">New password</label>
-                <input
-                  type="text"
-                  id="newnewpasswordname"
-                  className="form-control"
-                  defaultValue={user.displayName}
-                />
-
-                <button className="btn btn-primary mt-3">Update</button>
-              </div>
-            </form>
-          </div>
-
-          <div className={`${state === 3 ? "active-content" : "content"}`}>
-            <div>
-              <h3>Help</h3>
-              <p>Having Trouble?</p>
-            </div>
-
-            <div>
-              <h3>FAQ</h3>
-              <p>VIew out Frequently asked questions</p>
-            </div>
-
-            <div>
-              <h3>Contact us</h3>
-              <p>Contact our support team, offer limited to premium users</p>
-            </div>
-
-            <div>
-              <h3>Report an issue</h3>
-              <p>Report what's not working well, so we can fix it</p>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <Navigate to="/" />
+      )}
+    </>
   );
 };
 
