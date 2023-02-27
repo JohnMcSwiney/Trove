3e20738acbf7be2420433ac9c8f0a9c202c1a116
goosefring <113211020+goosefring@users.noreply.github.com>
2023-02-26 20:20:33 -0700

    adding files for artist domain

---
+++ album model/album-model.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const albumSchema = new mongoose.Schema({
+
+    albumName: {
+        type: String,
+        required: [true, `Please provide album's name`],
+        maxlength: 75,
+    },
+
+    albumArt: {
+        type: String,
+    },
+
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Artist',
+        required: true
+    },
+
+    featuredArtists: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Artist',
+            default: null
+        }
+    ],
+
+    totalTracks: {
+        type: Number,
+        default: 0,
+        required: [true, `Please provide number of tracks`],
+    },
+
+    isPublished: {
+        type: Boolean,
+        require: true
+    },
+
+    publishDate: {
+        type: Date,
+    },
+
+    releaseType: {
+        type: String,
+        enum: ['Album', 'EP', 'Single']
+    },
+    releaseYear: {
+        type: Number
+    },
+    songList: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Song',
+            default: null
+        }
+    ]
+})
+
+module.exports = mongoose.model('Album', albumSchema);
