b2c836d64fa974b8ff0f18a9a4507ae542375505
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-02-28 18:54:25 -0700

    Update useAuthContext.js

--- src/hooks/user-hooks/useAuthContext.js
+++ src/hooks/user-hooks/useAuthContext.js
@@ -1,11 +1,11 @@
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext } from "react";
 
-export const useAuthContext = ()=> {
-    const context = useContext(AuthContext);
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
 
-    if(!context){
-        throw Error ('useAuthContext must be used inside of AuthContextProvider')
-    }
-    return context
-}
+  if (!context) {
+    throw Error("useAuthContext must be used inside of AuthContextProvider");
+  }
+  return context;
+};
