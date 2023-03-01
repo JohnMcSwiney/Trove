5a8971e2bfc93e85aff398b5f9b0320d67c21be7
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-02-28 18:53:25 -0700

    tiny update

--- models/user model/user-model.js
+++ models/user model/user-model.js
@@ -107,13 +107,13 @@ userSchema.statics.login = async function (email, password) {
   const user = await this.findOne({ email });
 
   if (!user) {
-    throw Error("Incorrect email");
+    throw Error("Incorrect email or password");
   }
 
   const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
-    throw Error("Incorrect password");
+    throw Error("Incorrect email or password");
   }
 
   return user;
