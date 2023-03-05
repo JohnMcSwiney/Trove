8197374dad8e8ff0e6f24ad2d94f387ff477223b
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-03-04 17:21:06 -0700

    update

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import Song from "../../components/song detail/Song";
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const [songs, setSongs] = React.useState(null);
+  React.useEffect(() => {
+    const fetchSongs = async () => {
+      const response = await fetch("/api/songs/");
+      const json = await response.json();
 
-export default Search
+      if (response.ok) {
+        setSongs(json);
+      }
+    };
+
+    fetchSongs();
+  }, []);
+  return (
+    <div className="search-page container">
+      <div className="songs">
+        {songs && songs.map((data) => <Song key={data._id} song={data} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
