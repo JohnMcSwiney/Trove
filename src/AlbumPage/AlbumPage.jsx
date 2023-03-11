23bd5199c92f8d9b9a7e4189e4110e0ace7ee2d3
jvakhan <97688346+jvakhan@users.noreply.github.com>
2023-03-11 14:45:40 -0700

    Working on using Album API for album page
    works, but album needs a song list to display as well.

--- src/AlbumPage/AlbumPage.jsx
+++ src/AlbumPage/AlbumPage.jsx
@@ -5,8 +5,45 @@ import './AlbumPage.css';
 import albumsongs from "../data/albumsongs.json"
 import AlbumSong from "./AlbumSong";
 
+//fetching
+import { useParams } from "react-router-dom";
+
 // User's Top Genres
 export default function AlbumPage() {
+    let { id } = useParams();
+
+
+    const [album, setAlbum] = React.useState(null);
+    React.useEffect(() => {
+        const fetchAlbums = async () => {
+          const albumResponse = await fetch(`/api/albums/${id}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          });
+          const albumJson = await albumResponse.json();
+          if (albumResponse.ok) {
+            setAlbum(albumJson);
+          }
+        };
+        fetchAlbums();
+      }, [id]);
+
+      const [artist, setArtist] = React.useState(null);
+      React.useEffect(() => {
+        const findArtist = async () => {
+          const response = await fetch(`/api/artists/${album.artist}`);
+          const json = await response.json();
+    
+          if (!response.ok) {
+            console.log(json.error);
+          }
+    
+          if (response.ok) {
+            setArtist(json);
+          }
+        };
+        findArtist();
+      }, [album]);
 
     return (
         <section>
@@ -15,16 +52,16 @@ export default function AlbumPage() {
             {/* ALBUM COVER / INFO */}
             <div className="album--info">
                 <div className="album--song--cover">
-                        <img src="../../assets/reccover.jpg" alt="albumcover"/>
+                        <img src={album && album.albumArt} alt="albumcover"/>
                 </div>
                 <div className="album--stats--info">
-                        <h3>Album Name</h3>
-                        <h6><span>POP</span></h6>
+                        <h3>{album && album.albumName}</h3>
+                        <h6><span>{album && album.albumGenre.toUpperCase()}</span></h6>
                        <div className="album--release--info">
-                        <h5>2014</h5>
-                        <h6>ALBUM</h6>
+                        <h5>{album && album.releaseYear}</h5>
+                        <h6>{album && album.releaseType}</h6>
                         </div>
-                        <h4>Artist Name</h4>
+                        <h4>{artist && artist.artistName}</h4>
                 </div>
             </div>
 
