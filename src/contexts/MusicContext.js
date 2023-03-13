deffd5b432bec831432739b347d6e290c5b43de9
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-03-12 21:10:48 -0600

    adding new stuff

---
+++ src/contexts/MusicContext.js
@@ -0,0 +1,77 @@
+import { createContext, useState } from "react";
+
+//create the context
+export const MusicContext = createContext();
+
+// create a provider component to wrap the application
+
+export const MusicProvider = ({ children }) => {
+  const [currentSong, setCurrentSong] = useState(null);
+  const [playlists, setPlaylists] = useState([]);
+
+  // function to update the currently playing song
+  const updateCurrentSong = (song) => {
+    setCurrentSong(song);
+  };
+
+  const updatePlaylists = (newPlaylists) => {
+    setPlaylists(newPlaylists);
+  };
+
+  const contextValue = {
+    currentSong,
+    updateCurrentSong,
+    playlists,
+    updatePlaylists,
+  };
+
+  return (
+    <MusicContext.Provider value={contextValue}>
+      {children}
+    </MusicContext.Provider>
+  );
+};
+
+// import React, { useContext } from "react";
+// import { MusicContext } from "./MusicContext";
+
+// const CurrentlyPlaying = () => {
+//   // Access the context value for the current song
+//   const { currentSong } = useContext(MusicContext);
+
+//   return (
+//     <div>
+//       {currentSong ? (
+//         <div>
+//           <h2>Currently playing:</h2>
+//           <p>{currentSong.title}</p>
+//         </div>
+//       ) : (
+//         <p>No song currently playing</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CurrentlyPlaying;
+
+// import React, { useContext } from "react";
+// import { MusicContext } from "./MusicContext";
+
+// const Playlists = () => {
+//   // Access the context value for the user's playlists
+//   const { playlists } = useContext(MusicContext);
+
+//   return (
+//     <div>
+//       <h2>My Playlists:</h2>
+//       <ul>
+//         {playlists.map((playlist) => (
+//           <li key={playlist.id}>{playlist.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Playlists;
--- src/pages/create playlist/CreatePlaylist.jsx
+++ src/pages/create playlist/CreatePlaylist.jsx
@@ -1,59 +1,54 @@
-import React from "react"
+import React from "react";
 
-import './CreatePlaylist.css';
-import albumsongs from "../../data/albumsongs.json"
+import "./CreatePlaylist.css";
+import albumsongs from "../../data/albumsongs.json";
 import PlaylistSong from "./PlaylistSong";
-import PopUp from "./Popup"; 
+import PopUp from "./Popup";
 
 // To create a playlist
 export default function CreatePlaylist(props) {
-    const default_album = "../assets/default_playlistcover.png";
-    const addSongImg = "../assets/addsongsymbol.png";
-    const removeSongImg = "../assets/xsongsymbol.png";
-    const [previewCover, setPreviewCover] = React.useState(default_album);
-    const [albumSongs, setAlbumSongs] = React.useState(albumsongs);
-    const [playlistSongList, setPlaylistSongList] = React.useState(albumsongs);
+  const user = localStorage.getItem("user");
+  const id = JSON.parse(user).id;
 
-    const [imageFile, setImageFile] = React.useState();
-    const handleImageFileChange = (e) => {
-        setImageFile(e.target.files[0]);
-        setPreviewCover(URL.createObjectURL(e.target.files[0]));
-  
-    };
+  const default_album = "../assets/default_playlistcover.png";
+  const addSongImg = "../assets/addsongsymbol.png";
+  const removeSongImg = "../assets/xsongsymbol.png";
+  const [previewCover, setPreviewCover] = React.useState(default_album);
+  const [albumSongs, setAlbumSongs] = React.useState(albumsongs);
+  const [playlistSongList, setPlaylistSongList] = React.useState(albumsongs);
 
-    function handleRemoveSong(song, songAction) {
-        console.log(song.idno);
+  const [imageFile, setImageFile] = React.useState();
+  const handleImageFileChange = (e) => {
+    setImageFile(e.target.files[0]);
+    setPreviewCover(URL.createObjectURL(e.target.files[0]));
+  };
 
-        if(songAction === "remove") {
-        const newList = playlistSongList.filter((item) => item.idno != song.idno);
+  function handleRemoveSong(song, songAction) {
+    console.log(song.idno);
 
-            setPlaylistSongList(newList);
-        
-        }
+    if (songAction === "remove") {
+      const newList = playlistSongList.filter((item) => item.idno != song.idno);
 
-        else if(songAction === "add") {
-            setPlaylistSongList(prevPlaylistSongs => [...prevPlaylistSongs, song]);
-        }
-
+      setPlaylistSongList(newList);
+    } else if (songAction === "add") {
+      setPlaylistSongList((prevPlaylistSongs) => [...prevPlaylistSongs, song]);
     }
+  }
 
-    const [modalIsOpen, setIsOpen] = React.useState(true);
-    
-    function togglePop() {
-        setIsOpen(prevModalIsOpen => !prevModalIsOpen);
-        console.log("OPENED!")
+  const [modalIsOpen, setIsOpen] = React.useState(true);
 
-    }
+  function togglePop() {
+    setIsOpen((prevModalIsOpen) => !prevModalIsOpen);
+    console.log("OPENED!");
+  }
 
+  function addPlaylistSongs(newSong) {
+    setAlbumSongs((prevAlbumSongs) => [...prevAlbumSongs, newSong]);
+  }
 
-    function addPlaylistSongs(newSong) {
-        setAlbumSongs(prevAlbumSongs => [...prevAlbumSongs, newSong]);
-    
-    }
-
-
-    return(
+  return (
     <section>
+<<<<<<< Updated upstream
         {/* PLAYLIST'S INFO */}
         {modalIsOpen ? <div className="createplaylist--popupmask"></div> : null }
         {modalIsOpen ? <PopUp togglePop={togglePop} addPlaylistSongs={addPlaylistSongs} albumSongs={albumSongs} songActionImg={addSongImg} handleRemoveSong={handleRemoveSong} /> : null}
@@ -113,9 +108,91 @@ export default function CreatePlaylist(props) {
 
             </div>
             </div>
+=======
+      {/* PLAYLIST'S INFO */}
+      {modalIsOpen ? <div className="createplaylist--popupmask"></div> : null}
+      {modalIsOpen ? (
+        <PopUp
+          togglePop={togglePop}
+          addPlaylistSongs={addPlaylistSongs}
+          albumSongs={albumSongs}
+          songActionImg={addSongImg}
+          handleRemoveSong={handleRemoveSong}
+        />
+      ) : null}
+      <div className="createplaylist--info">
+        <div className="createplaylist--song--cover">
+          <img src={previewCover} alt="playlist" />
+          <label className="createplaylist--custom-file-upload">
+            <input
+              type="file"
+              name="cover"
+              value=""
+              accept="image/*"
+              className="createplaylist--gradient--btn createplaylist--image--btn createplaylist--hide--file"
+              onChange={handleImageFileChange}
+            />
+            Choose Image{" "}
+            <img
+              src="../../assets/upload_icon.png"
+              id="upload--icon"
+              alt="upload_icon"
+            />
+          </label>
+        </div>
+        <div className="createplaylist--stats--info">
+          <div className="createplaylist--createbtn">
+            <input
+              type="submit"
+              value="Create"
+              className="createplaylist--gradient--btn createplaylist--submit--btn"
+            />
+          </div>
+          <input type="text" id="playlisttitle" placeholder="Playlist Name" />
+          <div className="createplaylist--release--info">
+            {/* <h5>2014</h5> */}
+            <input
+              type="textarea"
+              id="playlistdesc"
+              placeholder="Add Description (Optional)"
+              name="playlist description"
+            />
+            {/* <h6>PLAYLIST</h6> */}
+          </div>
+          {/* <h4>Creator Username</h4> */}
+        </div>
+      </div>
 
+      <div className="createplaylist--addsongs">
+        <div className="createplaylist--searchbar">
+          {/* <input type="text" id="searchbar" placeholder="Search Songs"/> */}
+          <input
+            type="button"
+            value="Add Songs"
+            className="createplaylist--gradient--btn createplaylist--addsongsbtn"
+            onClick={togglePop}
+          />
+          <div className="createplaylist--addedsongs"></div>
+        </div>
+>>>>>>> Stashed changes
+
+        <div className="createplaylist--songs">
+          {playlistSongList &&
+            playlistSongList.map((item, index) => {
+              return (
+                <PlaylistSong
+                  key={index}
+                  {...item}
+                  song={item}
+                  index={index}
+                  handleRemoveSong={handleRemoveSong}
+                  songActionImg={removeSongImg}
+                  songAction={"remove"}
+                />
+              );
+            })}
+        </div>
+      </div>
     </section>
-
-    )
-
+  );
 }
