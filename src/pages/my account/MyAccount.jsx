9e33248eafd4b5a14d8aa4cd5a2589a772f96630
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-03-18 20:28:28 -0600

    MyAccountStyling

--- src/pages/my account/MyAccount.jsx
+++ src/pages/my account/MyAccount.jsx
@@ -82,10 +82,10 @@ const MyAccount = () => {
     }
   }
   return (
-    <div className='container'>
+    <div className='container '>
       <h1 className='my-account-header'>Settings</h1>
 
-      <div className='my-account bg-fglass-2'>
+      <div className='my-account bg-fglass-2 hideOverflow'>
         <div className='tabs bg-fglass-1'>
           <div className={`${state === 1 ? 'active-tab' : 'tab'}`}>
             <div className='account-display' onClick={() => action(1)}>
@@ -110,135 +110,140 @@ const MyAccount = () => {
 
         <div className='contents '>
           <div className={`${state === 1 ? 'active-content' : 'content'}`}>
-
-              <div className='personalInfoCont'>
-                <h3>Your personal information</h3>
-                <form onSubmit={handleUpdateAccount}>
-                  <div className='inner-form'>
-                    <div className='inputContainer'>
-                      <div className='userimg_displayName'>
-                        <div className='userImg-border'>
-                          <img
-                            src={imgPath}
-                            className='user-avatar'
-                            alt='your-avatar'
-                          />
-                        </div>
-                        <div className='dispNameInp'>
-                          <label htmlFor='name'>Display name:</label>
-                          <input
-                            type='text'
-                            id='name'
-                            className='form-control'
-                            defaultValue={displayName}
-                            onChange={e => setDisPlayName(e.target.value)}
-                            value={displayName}
-                          />
-                        </div>
-                      </div>
-
-                      <div className='emailImpCont'>
-                        <label htmlFor='email'>Email:</label>
-                        <input
-                          type='email'
-                          id='email'
-                          className='form-control'
-                          defaultValue={email}
-                          disabled
-                        />
-                      </div>
-
+            <div className='personalInfoCont'>
+              <h3>Your personal information</h3>
+              <form onSubmit={handleUpdateAccount}>
+                <div className='inner-form'>
+                  <div className='inputContainer'>
+                    <div className='userimg_displayName'>
                       <div>
-                        <label htmlFor='dob'>Date of birth:</label>
-                        <input
-                          type='date'
-                          id='dob'
-                          name='dob'
-                          // defaultValue={time}
-                          onChange={e => setDOB(e.target.value)}
-                          value={dob}
-                          className='form-control'
+                      <div className='userImg-border'>
+                        <img
+                          src={imgPath}
+                          className='user-avatar'
+                          alt='your-avatar'
                         />
                       </div>
-                      <button className='btn btn-primary mt-3'>Save</button>
+                      </div>
+                      <div className='dispNameInp'>
+                        <label htmlFor='name'>Display name:</label>
+                        <input
+                          type='text'
+                          id='name'
+                          className='form-control'
+                          defaultValue={displayName}
+                          onChange={e => setDisPlayName(e.target.value)}
+                          value={displayName}
+                        />
+                      </div>
                     </div>
-                  </div>
-                </form>
-              </div>
 
-            
-            
+                    <div className='emailImpCont'>
+                      <label htmlFor='email'>Email:</label>
+                      <input
+                        type='email'
+                        id='email'
+                        className='form-control'
+                        defaultValue={email}
+                        disabled
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor='dob'>Date of birth:</label>
+                      <input
+                        type='date'
+                        id='dob'
+                        name='dob'
+                        // defaultValue={time}
+                        onChange={e => setDOB(e.target.value)}
+                        value={dob}
+                        className='form-control'
+                      />
+                    </div>
+                    <button className='btn btn-primary mt-3'>Save</button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
 
           <div className={`${state === 2 ? 'active-content' : 'content'}`}>
             <h3>Change your personal account information</h3>
+            <div className='updateMailPassCont'>
+              <div className='updateMailPassinput'>
+              <form onSubmit={handleUpdateEmail}>
+                <h5 onClick={toggleShowEmail}>Email</h5>
+                {showEmailTab && (
+                  <div>
+                    <label htmlFor='newemail'>New email</label>
+                    <input
+                      type='email'
+                      id='newemail'
+                      className='form-control'
+                      placeholder='Change your email here'
+                      onChange={e => setNewEmail(e.target.value)}
+                      value={newEmail}
+                    />
 
-            <form onSubmit={handleUpdateEmail}>
-              <h5 onClick={toggleShowEmail}>Email</h5>
-              {showEmailTab && (
-                <div>
-                  <label htmlFor='newemail'>New email</label>
-                  <input
-                    type='email'
-                    id='newemail'
-                    className='form-control'
-                    placeholder='Change your email here'
-                    onChange={e => setNewEmail(e.target.value)}
-                    value={newEmail}
-                  />
+                    <label htmlFor='Cpassword'>Confirm password</label>
+                    <input
+                      type={'password'}
+                      id='Cpassword'
+                      className='form-control'
+                      placeholder='Current password'
+                      onChange={e => setCPassword(e.target.value)}
+                      value={cPassword}
+                    />
 
-                  <label htmlFor='Cpassword'>Confirm password</label>
-                  <input
-                    type={'password'}
-                    id='Cpassword'
-                    className='form-control'
-                    placeholder='Current password'
-                    onChange={e => setCPassword(e.target.value)}
-                    value={cPassword}
-                  />
+                    <button className='btn btn-primary mt-3 mb-3'>
+                      Update Email
+                    </button>
+                  </div>
+                )}
+              </form>
+              </div>
+              
+              
+              <div className='updateMailPassinput'>
+              <form onSubmit={handleUpdatePassword}>
+                <h5 onClick={toggleShowPasswordTab}>Password</h5>
+                {showPasswordTab && (
+                  <div>
+                    <label htmlFor='password'>Current password</label>
+                    <input
+                      type={'password'}
+                      id='password'
+                      className='form-control'
+                      placeholder='Current password'
+                      onChange={e => setPassword(e.target.value)}
+                      value={password}
+                    />
 
-                  <button className='btn btn-primary mt-3 mb-3'>
-                    Update Email
-                  </button>
-                </div>
-              )}
-            </form>
+                    <label htmlFor='newpassword'>New password</label>
+                    {showPassword ? (
+                      <BiShow onClick={toggleHidden} />
+                    ) : (
+                      <AiOutlineEyeInvisible onClick={toggleHidden} />
+                    )}
+                    <input
+                      type={showPassword ? 'text' : 'password'}
+                      id='newpassword'
+                      className='form-control'
+                      placeholder='New password'
+                      onChange={e => setNewPassword(e.target.value)}
+                      value={newPassword}
+                    />
 
-            <form onSubmit={handleUpdatePassword}>
-              <h5 onClick={toggleShowPasswordTab}>Password</h5>
-              {showPasswordTab && (
-                <div>
-                  <label htmlFor='password'>Current password</label>
-                  <input
-                    type={'password'}
-                    id='password'
-                    className='form-control'
-                    placeholder='Current password'
-                    onChange={e => setPassword(e.target.value)}
-                    value={password}
-                  />
-
-                  <label htmlFor='newpassword'>New password</label>
-                  {showPassword ? (
-                    <BiShow onClick={toggleHidden} />
-                  ) : (
-                    <AiOutlineEyeInvisible onClick={toggleHidden} />
-                  )}
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    id='newpassword'
-                    className='form-control'
-                    placeholder='New password'
-                    onChange={e => setNewPassword(e.target.value)}
-                    value={newPassword}
-                  />
-
-                  <button className='btn btn-primary mt-3'>
-                    Change Password
-                  </button>
-                </div>
-              )}
-            </form>
+                    <button className='btn btn-primary mt-3'>
+                      Change Password
+                    </button>
+                  </div>
+                )}
+              </form>
+              </div>
+              
+            </div>
           </div>
 
           <div className={`${state === 3 ? 'active-content' : 'content'}`}>
