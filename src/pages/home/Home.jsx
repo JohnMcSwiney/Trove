31aa5cf525b6664aead46782b86fc3de1d762418
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-03-22 10:29:55 -0600

    fixed again

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,8 @@ import { useArtistAuthContext } from "../../hooks/useArtistAuthContext";
 const Home = () => {
   const [artistSongs, setArtistSongs] = React.useState(null);
   const [artistAlbums, setArtistAlbum] = React.useState(null);
-  const artistID = localStorage.getItem("artist");
-  const id = artistID ? JSON.stringify(artistID) : null;
+  const artistID = JSON.parse(localStorage.getItem("artist"));
+  const id = artistID ? artistID.id : null;
   React.useEffect(() => {
     const fetchMySongs = async () => {
       const response = await fetch(`/api/songs/artist-songs/${id}`, {
