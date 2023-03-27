973243c1cbdfe45bc9646f74062a0edb117cb5f3
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-03-27 02:58:49 -0600

    update

--- src/components/modals/user modal/UserModal.jsx
+++ src/components/modals/user modal/UserModal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import "./editModal.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { useEditUser } from "../../../hooks/useEditUser";
 const UserModal = ({ user }) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -10,10 +11,26 @@ const UserModal = ({ user }) => {
   const [password, setPassword] = React.useState(user?.password);
   const [avatar, setAvatar] = React.useState(user?.imageURL);
   const [dob, setDOB] = React.useState(user?.dob);
-  console.log(dob);
+
   const [show, setShow] = React.useState(false);
+
+  const { editUser, message, editerror, editIsLoading } = useEditUser();
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("edit");
+      await editUser(user._id, avatar, displayName, dob, email, password);
+    } catch (error) {
+      console.log(error?.message || "Please try again");
+      return;
+    }
+  };
+
+  const handleDelete = () => {
+    console.log("deleted");
+  };
   return (
-    <form>
+    <form onSubmit={handleUpdate}>
       <Button variant="primary" onClick={handleShow}>
         Edit User Info
       </Button>
@@ -28,58 +45,69 @@ const UserModal = ({ user }) => {
           <Modal.Title>Edit User</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <label htmlFor="avatar"> IMG: </label>
-          <img
-            id="avatar"
-            src={avatar}
-            alt={user?.displayName}
-            width={"50px"}
-          />
-          <br />
-          <label htmlFor="displayName">DisplayName: </label>
-          <input
-            type="text"
-            id="displayName"
-            value={displayName}
-            onChange={(e) => setDisplayName(e.target.value)}
-            className="form-control"
-          ></input>
+          <div className="form-group">
+            <label htmlFor="avatar"> IMG: </label>
+            <img
+              id="avatar"
+              src={avatar}
+              alt={user?.displayName}
+              width={"50px"}
+            />
+            <br />
+            <label htmlFor="displayName">DisplayName: </label>
+            <input
+              type="text"
+              id="displayName"
+              value={displayName}
+              onChange={(e) => setDisplayName(e.target.value)}
+              className="form-control"
+            ></input>
 
-          <label htmlFor="dob">Date of birth: </label>
-          <input
-            type="date"
-            id="dob"
-            value={dob}
-            onChange={(e) => setDOB(e.target.value)}
-            className="form-control"
-          ></input>
-          <label htmlFor="email">Email </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-          ></input>
+            <label htmlFor="dob">Date of birth: </label>
+            <input
+              type="date"
+              id="dob"
+              value={dob}
+              onChange={(e) => setDOB(e.target.value)}
+              className="form-control"
+            ></input>
+            <label htmlFor="email">Email </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+            ></input>
 
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-control"
-          ></input>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+            ></input>
+          </div>
         </Modal.Body>
-        <div className="form-group">
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="danger">Delete User</Button>
-            <Button variant="primary">Update User</Button>
-          </Modal.Footer>
-        </div>
+
+        <Modal.Footer>
+          {editerror && <div className="error">{editerror}</div>}
+          {message && <div className="message">{message}</div>}
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <button className="btn btn-danger" onClick={handleDelete}>
+            Delete User
+          </button>
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={handleUpdate}
+          >
+            Update User
+          </button>
+        </Modal.Footer>
       </Modal>
     </form>
   );
---
+++ src/hooks/useEditUser.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const useEditUser = () => {
+  const [editIsLoading, setEditIsLoading] = useState(false);
+  const [editerror, setEditError] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const editUser = async (id, avatar, displayName, dob, email, password) => {
+    setEditError(null);
+    setEditIsLoading(true);
+    const response = await fetch(`/api/users/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ displayName, avatar, dob, email, password }),
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      setEditError(json.error);
+    }
+
+    if (response.ok) {
+      setMessage(json.message);
+    }
+    setEditIsLoading(false);
+  };
+  return { editUser, message, editerror, editIsLoading };
+};
