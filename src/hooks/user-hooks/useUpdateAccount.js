2f8be8c7ce52cc0ce876b93e199090ee0347a71d
jvakhan <97688346+jvakhan@users.noreply.github.com>
2023-04-01 12:00:21 -0600

    Update User Profile Photo works

--- src/hooks/user-hooks/useUpdateAccount.js
+++ src/hooks/user-hooks/useUpdateAccount.js
@@ -1,11 +1,94 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import firebase from "../../pages/my account/firebaseConfig";
 
 export const useUpdateAccount = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [updateMessage, setUpdateMessage] = useState("");
-  const updateAccount = async (displayName, dob) => {
+
+  // profile photo
+  const storage = firebase.storage();
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  let profilePhotoUrl ="";
+  
+  const updateAccount = async (
+    displayName, 
+    dob,
+    imageFile
+  ) => {
+
+    const storageRef = storage.ref();
+
+    const uploadImageToFirebase = async () => {
+
+      let imageCounter = 0;
+
+      while (await checkImageExists(imageCounter)) {
+        imageCounter++;
+      }
+
+      const imageRef = storageRef.child(`images/${imageCounter}`);
+
+      const imageUploadTask = imageRef.put(imageFile);
+
+      console.log("imageFile: " + imageFile);
+
+      console.log("imageFile[0]: " + imageFile[0]);
+
+      console.log("imageRef: " + imageRef);
+
+      return new Promise((resolve, reject) => {
+        imageUploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            setUploadProgress(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+
+            console.log("Bytes Transferred: " + snapshot.bytesTransferred);
+
+            console.log("Total Bytes: " + snapshot.totalBytes);
+
+            console.log(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+          },
+          (err) => {
+            console.log(err);
+            reject(err);
+          },
+          async () => {
+            const profilePhotoUrl = await imageRef.getDownloadURL();
+            resolve(profilePhotoUrl);
+
+            console.log("finished handlingimg");
+          }
+        );
+      });
+    };
+
+    const checkImageExists = async (imageCounter) => {
+
+      const imageRef = storageRef.child(`images/${imageCounter}`);
+
+      const metadata = await imageRef.getMetadata()
+      .catch(err => {
+
+        if (err.code === "storage/object-not-found") {
+          return false;
+        }
+        else {
+          console.log(err);
+          return true;
+        }
+      });
+      return metadata !== false
+    }
+
+
+  const updateUserObject = async (displayName, dob, imageURL) => {
     setIsLoading(true);
     setError(null);
 
@@ -14,7 +97,12 @@ export const useUpdateAccount = () => {
     const response = await fetch(`/api/users/ua/${userId}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ displayName, dob }),
+      body: JSON.stringify
+      ({ 
+        displayName, 
+        dob, 
+        imageURL
+      }),
     });
 
     const json = await response.json();
@@ -30,5 +118,13 @@ export const useUpdateAccount = () => {
     localStorage.setItem("user", JSON.stringify(user));
   };
 
+  if(imageFile) {
+    profilePhotoUrl = await uploadImageToFirebase();
+  }
+  
+  await updateUserObject(displayName, dob, profilePhotoUrl);
+
+  };
   return { updateAccount, error, isLoading, updateMessage };
+
 };
