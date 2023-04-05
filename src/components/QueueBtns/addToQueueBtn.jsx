9212e9da3ced64c5138c24865c617576dcbc1a8f
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-04-05 09:41:35 -0600

    AddToQueuePopup added

--- src/components/QueueBtns/addToQueueBtn.jsx
+++ src/components/QueueBtns/addToQueueBtn.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MusicContext } from '../../contexts/MusicContext'
+import {IoIosAdd} from 'react-icons/io'
+import './addToQueueBtn.css'
+
 
 export default function AddToQueueBtn (song) {
   const {
@@ -17,23 +20,50 @@ export default function AddToQueueBtn (song) {
   } = React.useContext(MusicContext)
 
   const appendQueue = () => {
+    
     console.log('adding to queue - component call')
     addToQueue(song);
     // if (play_list.length > 0) {
       
     // }
   }
+  const [isActive, setIsActive] = useState(false);
 
+  useEffect(() => {
+    if (isActive) {
+      setTimeout(() => {
+        setIsActive(false);
+      }, 700);
+    }
+  }, [isActive]);
+
+  const handleClick = () => {
+    setIsActive(true);
+  };
   return (
-    <div>
+    <div className={`addQueueCont ${isActive ? "addQueueClicked" : ""}`}
+    onClick={handleClick}
+    >
       <button
-        className='btn'
+        className='addQueueBtn'
         onClick={() => {
           appendQueue()
         }}
       >
-        + queue
+        <IoIosAdd/>
+        
+        
       </button>
+      <div className='addQueueTxt'>
+       <p 
+       className='
+       addQueueAddQuery
+       '>Add to queue</p>
+       <p 
+       className='
+       addQueueAdded
+       '>Added!</p>
+        </div>
       
     </div>
   )
