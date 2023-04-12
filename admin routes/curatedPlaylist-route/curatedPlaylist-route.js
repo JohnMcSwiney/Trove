8803e4aace8736865cc4029f96b1aec3aacc56da
Johnny Mcswiney <johnnymcswiney64@gmail.com>
2023-04-12 09:33:30 -0600

    curated

--- admin routes/curatedPlaylist-route/curatedPlaylist-route.js
+++ admin routes/curatedPlaylist-route/curatedPlaylist-route.js
@@ -1,44 +1,44 @@
-// const express = require('express');
-// const router = express.Router();
-// const cron = require("node-cron");
-// const CuratedPlaylist = require("../../models/curatedPlaylist model/curatedPlaylist-model");
+const express = require('express');
+const router = express.Router();
+const cron = require("node-cron");
+const CuratedPlaylist = require("../../models/curatedPlaylist model/curatedPlaylist-model");
 
 
-// const {
-//     getAllCuratedPlaylist,
-//     getACuratedPlaylist,
-//     createCuratedPlaylist,
-//     getRandomCuratedPlaylist,
-//     generateCuratedPlaylists,
-//     updateCuratedPlaylist,
-//     deleteCuratedPlaylist,
-// } = require('../../admin controllers/curatedPlaylist/curatedPlaylistController');
+const {
+    getAllCuratedPlaylist,
+    getACuratedPlaylist,
+    createCuratedPlaylist,
+    getRandomCuratedPlaylist,
+    generateCuratedPlaylists,
+    updateCuratedPlaylist,
+    deleteCuratedPlaylist,
+} = require('../../admin controllers/curatedPlaylist/curatedPlaylistController');
 
-// router.get('/', getAllCuratedPlaylist);
+router.get('/', getAllCuratedPlaylist);
 
-// router.get('/:id', getACuratedPlaylist);
+router.get('/:id', getACuratedPlaylist);
 
-// router.post('/', createCuratedPlaylist);
+router.post('/', createCuratedPlaylist);
 
-// router.post('/trove-picks', generateCuratedPlaylists);
-
-
-// // router.post('/', generateRandomCuratedPlaylist);
+router.post('/trove-picks', generateCuratedPlaylists);
 
 
-// router.patch('/:id', updateCuratedPlaylist);
+// router.post('/', generateRandomCuratedPlaylist);
 
-// router.delete('/:id', deleteCuratedPlaylist);
 
-// cron.schedule("0 0 * * *", async () => {
-//     try {
-//         await CuratedPlaylist.deleteMany({ isGenerated: true });
-//         await generateCuratedPlaylists();
-//         console.log("generated curated playlists!");
-//     } catch (err) {
-//         console.log(err);
-//         res.status(400).json({ message: err.message });
-//     }
-// });
+router.patch('/:id', updateCuratedPlaylist);
 
-// module.exports = router;
+router.delete('/:id', deleteCuratedPlaylist);
+
+cron.schedule("0 0 * * *", async () => {
+    try {
+        await CuratedPlaylist.deleteMany({ isGenerated: true });
+        await generateCuratedPlaylists();
+        console.log("generated curated playlists!");
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: err.message });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const userRouter = require("./admin routes/user-route/user-route");
 const albumRouter = require("./admin routes/album-route/album-route");
 const artistRouter = require("./admin routes/artist-route/artist-route");
-// const curatedPlaylistRouter = require("./admin routes/curatedPlaylist-route/curatedPlaylist-route");
+const curatedPlaylistRouter = require("./admin routes/curatedPlaylist-route/curatedPlaylist-route");
 
 // const tastepRouter = require("./admin routes/tastep-route/tastep-route");
 const playlistRouter = require("./admin routes/playlist-route/playlist-route");
@@ -90,7 +90,7 @@ app.use("/api/albums", albumRouter);
 app.use("/api/artists", artistRouter);
 
 //curated playlists
-// app.use("/api/curated", curatedPlaylistRouter);
+app.use("/api/curated", curatedPlaylistRouter);
 
 // //playlist
 app.use("/api/playlists", playlistRouter);
