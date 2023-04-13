b53770deeac87d80a34cf716cf947bf4e4688c4d
Dan1Nguyen01 <dannguyen062601@gmail.com>
2023-04-13 16:29:35 -0600

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     DB_URL: "mongodb+srv://admin:trovepassword@trove.dguzpr3.mongodb.net/?retryWrites=true&w=majority",
     PORT: 8080,
     SECRET: "randombull",
-    PREFIX: '/',
+    PREFIX: '/api/',
     GOOGLE_USER: "dannguyen0826@gmail.com",
     GOOGLE_PASSWORD:"rbrtlmmcotupmmzz"
 }
