8f44bf87749c045fa48577d179402d442aae81a7
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-04-14 14:31:10 -0600

    update

--- src/components/cards/card_discoverygame/CardDiscovery.jsx
+++ src/components/cards/card_discoverygame/CardDiscovery.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import './CardDiscovery.css'
+import { useAuthContext } from "../../../hooks/user-hooks/useAuthContext";
+import "./CardDiscovery.css";
+import React from "react";
 
-export default function discovery_card () {
+const discovery_card = () => {
   return (
-    <div className='DG-card-container'>
-      <div className='DG-bg-Img'>
-        <div className='DG-title-card'>
-          <h1 className='DG_title'><strong>Discovery </strong>Game</h1>
+    <div className="DG-card-container">
+      <div className="DG-bg-Img">
+        <div className="DG-title-card">
+          <h1 className="DG_title">
+            <strong>Discovery </strong>Game
+          </h1>
         </div>
       </div>
     </div>
-  )
+  );
 };
+
+export default discovery_card;
